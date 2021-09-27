d2a95e35ba2fd558139b4a79a81fac13f123f770
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-27 12:19:12 +0530

    Profit & Loss logic update

--- app.js
+++ app.js
@@ -4,3 +4,27 @@ const currentPrice = document.querySelector("#current-price");
 const submitButton = document.querySelector("#submit-button");
 const showMessage = document.querySelector("#output");
 
+function calculateProfitAndLoss(){
+    const initial = initialPrice.value;
+    const qty = qtyStock.value;
+    const curprice = currentPrice.value;
+
+    if(curprice > initial)
+    {
+        const profitpershare = curprice-initial;
+        const totalprofit = profitpershare*qty;
+        const profitPercentage = (profitpershare/initial)*100;
+        console.log(totalprofit);
+        console.log(profitPercentage);
+    } else if(initial > curprice) {
+        const losspershare = initial-curprice;
+        const totalloss = losspershare*qty;
+        const lossPercentage = (losspershare/initial)*100;
+        console.log(totalloss);
+        console.log(lossPercentage);
+    } else {
+        console.log("no pain no gain");
+    }
+}
+
+submitButton.addEventListener("click", calculateProfitAndLoss);
