90d0a18cc07f3414f07335507eac3d89360230cf
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-27 11:38:12 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const initialPrice = document.querySelector("#initial-price");
+const qtyStock = document.querySelector("#qty-stock");
+const currentPrice = document.querySelector("#current-price");
+const submitButton = document.querySelector("#submit-button");
+const showMessage = document.querySelector("#output");
+
