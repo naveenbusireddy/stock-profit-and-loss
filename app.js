3cefe5751fdbff8ca7929ad1ae27b6dadc8c3922
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-28 17:33:38 +0530

    Fix: Input Negative values

--- app.js
+++ app.js
@@ -9,29 +9,34 @@ function calculateProfitAndLoss() {
     const qty = Number(qtyStock.value);
     const curprice = Number(currentPrice.value);
 
-    if (initial&&qty&&curprice) {
-        if(curprice > initial)
-    {
-        const profit = curprice-initial;
-        const totalprofit = profit*qty;
-        const profitPercentage = (profit/initial)*100;        
-        displayMessage('The total profit is:' +totalprofit+ " and profit percentage is: " +profitPercentage.toFixed(2)+'%'); //numdigit.toFixed(2)
-
-    } else if(initial > curprice) {
-            const loss = initial-curprice;
-            const totalloss = loss*qty;
-            const lossPercentage = ((loss/initial)*100);
-            displayMessage('The total loss is:' +totalloss+ ' and loss percentage is:' +lossPercentage.toFixed(2)+'%');
-    
-        } else {
-            displayMessage('No pain no gain, no gain no pain');
-        }        
+    if (initial < 0 || qty < 0 || curprice < 0) {
+        alert("Please enter the positive values");        
     }
-    else {
-        displayMessage('Please enter the three values');
-    }
+    else { 
+        if(initial===0 || qty===0 || curprice===0 )
+        {
+            displayMessage('Zero values not acceptable');
+        }
+        else {
+            if(curprice > initial)
+            {
+                const profit = curprice-initial;
+                const totalprofit = profit*qty;
+                const profitPercentage = (profit/initial)*100;        
+                displayMessage('The total profit is:' +totalprofit+ " and profit percentage is: " +profitPercentage.toFixed(2)+'%'); //numdigit.toFixed(2)
 
-    
+            } else if(initial > curprice) {
+                const loss = initial-curprice;
+                const totalloss = loss*qty;
+                const lossPercentage = ((loss/initial)*100);
+                displayMessage('The total loss is:' +totalloss+ ' and loss percentage is:' +lossPercentage.toFixed(2)+'%');
+        
+            } else {
+                displayMessage('No pain no gain, no gain no pain');
+            }
+        }   
+               
+    }    
 }
 
 submitButton.addEventListener("click", calculateProfitAndLoss);
